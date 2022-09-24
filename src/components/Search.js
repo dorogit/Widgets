e8b270e3d961716416d98e6e9cc18663155c912c
dorogit <ishaanjainlock10@gmail.com>
2022-09-24 16:38:23 +0530

    added more stuff

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,36 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import wikiResult from "../API/wikipedia";
 
 const Search = () => {
-  const [searchTerm,setSearchTerm] = useState(null)
-
+  const [searchTerm,setSearchTerm] = useState('hello')
+  const [responseSummary,setResponseSummary] = useState([]) 
+  console.log(responseSummary)
   useEffect(()=> {
-    console.log('epic')
-  }, [searchTerm])
+    const search = async () => {
+      const response = await axios.get('https://en.wikipedia.org/w/api.php',{
+        params:{
+          action:'query',
+          list: 'search',
+          origin: '*',
+          format:'json',
+          srsearch: searchTerm
+        }
+      })
+      setResponseSummary(response.data.query.search)
+    }
+    if (searchTerm) {
+      search()
+    }
+  },[searchTerm])
+
+  const renderedList = responseSummary.map((response) => {
+    return (
+      <div>
+        {response.snippet}
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -15,6 +39,7 @@ const Search = () => {
           Enter search term
         </label>
         <input value = {searchTerm} className="input" onChange={(entered) =>setSearchTerm(entered.target.value)}/>
+        {renderedList}
       </div>
     </div>
   )
